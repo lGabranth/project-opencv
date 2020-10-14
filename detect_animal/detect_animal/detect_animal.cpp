73e44ad5565079a26014a55e734a3f322078ec32
Charles Bourtoire <charlesbourtoire@gmail.com>
2020-10-14 12:22:54 +0200

    detect-visage fonctionnel

--- detect_animal/detect_animal/detect_animal.cpp
+++ detect_animal/detect_animal/detect_animal.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>  
 #include <iostream>
+#include "FaceDetector.hpp"
 
 using namespace cv;
 using namespace std;
@@ -9,20 +10,23 @@ int main(int argc, char** argv) {
     if (!video_capture.open(0)) {
         return 0;
     }
-
+    FaceDetector face_detector;
     cv::Mat frame;
     while (true) {
         video_capture >> frame;
-
+        auto rectangles = face_detector.detect_face_rectangles(frame);
+        cv::Scalar color(0, 105, 205);
+        int frame_thickness = 4;
+        for (const auto& r : rectangles) {
+            cv::rectangle(frame, r, color, frame_thickness);
+        }
         imshow("Image", frame);
         const int esc_key = 27;
         if (cv::waitKey(10) == esc_key) {
             break;
         }
     }
-
     cv::destroyAllWindows();
     video_capture.release();
-
     return 0;
 }
