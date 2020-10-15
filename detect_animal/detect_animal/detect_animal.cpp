1a969d7aa7701f6bbb48e4dd3041d95a839b36d6
Charles Bourtoire <charlesbourtoire@gmail.com>
2020-10-15 16:13:17 +0200

    Optimisation code + texte sur frame

--- detect_animal/detect_animal/detect_animal.cpp
+++ detect_animal/detect_animal/detect_animal.cpp
@@ -1,13 +1,12 @@
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
+#include <opencv2/opencv.hpp>
 #include <iostream>
+
 using namespace std;
 using namespace cv;
+
 void detectAndDisplay(Mat frame);
+
 CascadeClassifier face_cascade;
-CascadeClassifier smile_cascade;
 CascadeClassifier nose_cascade;
 CascadeClassifier mouth_cascade;
 
@@ -15,47 +14,41 @@ int main(int argc, const char** argv)
 {
     CommandLineParser parser(argc, argv,
         "{help h||}"
-        "{face_cascade|data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
-        "{smile_cascade|data/haarcascades/haarcascade_smile.xml|Path to smile cascade.}"
-        "{nose_cascade|data/haarcascades/haarcascade_mcs_nose.xml|Path to nose cascade.}"
-        "{mouth_cascade|data/haarcascades/haarcascade_mcs_mouth.xml|Path to mouth cascade.}"
-        "{camera|0|Camera device number.}");
-    parser.about("\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
-        "You can use Haar or LBP features.\n\n");
+        "{face_cascade|data/haarcascades/haarcascade_frontalface_alt.xml|Chemin de la cascade des visages.}"
+        "{nose_cascade|data/haarcascades/haarcascade_mcs_nose.xml|Chemin de la cascade des nezs.}"
+        "{mouth_cascade|data/haarcascades/haarcascade_mcs_mouth.xml|Chemin de la cascade des bouches.}"
+        "{camera|0|Numero de la camera.}");
+    parser.about("\nCe programme détecte si une personne porte son masque correctement ou non.\n\n");
     parser.printMessage();
+
     String face_cascade_name = samples::findFile(parser.get<String>("face_cascade"));
-    String smile_cascade_name = samples::findFile(parser.get<String>("smile_cascade"));
     String nose_cascade_name = samples::findFile(parser.get<String>("nose_cascade"));
     String mouth_cascade_name = samples::findFile(parser.get<String>("mouth_cascade"));
 
-    //-- 1. Load the cascades
+    // On charge toutes les "cascades"
     if (!face_cascade.load(face_cascade_name))
     {
-        cout << "--(!)Error loading face cascade\n";
-        return -1;
-    };
-    if (!smile_cascade.load(smile_cascade_name))
-    {
-        cout << "--(!)Error loading smile cascade\n";
+        cout << "/!\\Erreur de chargement lors de la cascade des visages\n";
         return -1;
     };
     if (!nose_cascade.load(nose_cascade_name))
     {
-        cout << "--(!)Error loading nose cascade\n";
+        cout << "/!\\Erreur de chargement lors de la cascade des nezs\n";
         return -1;
     };
     if (!mouth_cascade.load(mouth_cascade_name))
     {
-        cout << "--(!)Error loading mouth cascade\n";
+        cout << "/!\\Erreur de chargement lors de la cascade des bouches\n";
         return -1;
     };
+
     int camera_device = parser.get<int>("camera");
     VideoCapture capture;
-    //-- 2. Read the video stream
+    // On lit le stream vidéo
     capture.open(camera_device);
     if (!capture.isOpened())
     {
-        cout << "--(!)Error opening video capture\n";
+        cout << "/!\\Erreur lors de l'ouverture de la camera\n";
         return -1;
     }
     Mat frame;
@@ -63,51 +56,58 @@ int main(int argc, const char** argv)
     {
         if (frame.empty())
         {
-            cout << "--(!) No captured frame -- Break!\n";
+            cout << "/!\\Pas d'image capturee\n";
             break;
         }
-        //-- 3. Apply the classifier to the frame
+        // Traitement de la vidéo
         detectAndDisplay(frame);
+        // Si on appuie sur le bouton "esc" pendant la vidéo, on quitte cette dernière.
         if (waitKey(10) == 27)
         {
-            break; // escape
+            break;
         }
     }
     return 0;
 }
+
 void detectAndDisplay(Mat frame)
 {
     Mat frame_gray;
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
-    //-- Detect faces
+    // Détection du visage
     std::vector<Rect> faces;
     face_cascade.detectMultiScale(frame_gray, faces);
-    Scalar *t = new Scalar;
-    
 
     for (size_t i = 0; i < faces.size(); i++)
     {
         Mat faceROI = frame_gray(faces[i]);
-        //-- In each face, detect eyes
+        // Sur chaque visage on tente de détecter le nez et la bouche
         std::vector<Rect> nose;
         nose_cascade.detectMultiScale(faceROI, nose);
         std::vector<Rect> mouth;
-        mouth_cascade.detectMultiScale(faceROI, mouth);
+        nose_cascade.detectMultiScale(faceROI, mouth);
 
+        // Afficher du texte sur l'image
+        int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
+        double fontScale = 1.5;
+        int thickness = 2;
+        Point textOrg(100, 100);
         Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-        if (nose.size() == 0) {
+        // Si on ne détecte ni le nez, ni la bouche ; on part du postulat que la personne porte son masque.
+        // Masque = visage entouré en vert
+        // Pas de masque = visage entouré en rouge
+        if (nose.size() == 0 || mouth.size() == 0) {
             ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(0, 255, 0), 4);
+            string someText = "Masque !";
+            putText(frame, someText, textOrg, fontFace, fontScale, Scalar(0, 255, 0), thickness, 8);
         }
         else {
             ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(0, 0, 255), 4);
+            string someText = "Pas de masque !";
+            putText(frame, someText, textOrg, fontFace, fontScale, Scalar(0, 0, 255), thickness, 8);
         }
-        //ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, color, 4);
-
-
-
     }
 
-    //-- Show what you got
-    imshow("Capture - Face detection", frame);
+    imshow("Capture - Detection de port du masque", frame);
 }
